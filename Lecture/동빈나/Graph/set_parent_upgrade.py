0da8238fc6c30f2acfc038e45196ccf2e1bf78cd
jsj0718 <jeongsejin718@gmail.com>
2020-12-15 18:24:13 +0900

    Learn about graph theory

---
+++ Lecture/동빈나/Graph/set_parent_upgrade.py
@@ -0,0 +1,42 @@
+# 경로 압축 방법 사용
+
+# 특정 원소가 속한 집합 찾기
+def find_parent(parent, x):
+    # 루트 노드를 찾을 때까지 재귀 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = parent[a]
+    else:
+        parent[a] = parent[b]
+
+# 노드의 개수와 간선(Union 연산)의 개수 입력 받기
+v, e = map(int, input().split())
+parent = [0] * (v + 1)  # 부모 테이블 0으로 초기화
+
+# 부모 테이블에서 부모를 자기 자신으로 초기화
+for i in range(1, v + 1):
+    parent[i] = i
+
+# Union 연산 수행
+for i in range(e):
+    a, b = map(int, input().split())
+    union_parent(parent, a, b)
+
+# 각 원소가 속한 집합 출력
+print("각 원소가 속한 집합: ", end='')
+for i in range(1, v + 1):
+    print(find_parent(parent, i), end=' ')
+
+print()
+
+# 부모 테이블 내용 출력
+print("부모 테이블 내용: ", end='')
+for i in range(1, v+ 1):
+    print(parent[i], end=' ')
